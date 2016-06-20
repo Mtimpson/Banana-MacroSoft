2c25baadb1d34cf087cecef798887d7c14f546b3
benbrott <ben.r.brott@gmail.com>
2016-06-20 18:10:53 -0400

    Constants file

---
+++ ForgottenTrail/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  ForgottenTrail
+//
+//  Created by Ben Brott on 6/20/16.
+//  Copyright © 2016 newBorn Software Development Company. All rights reserved.
+//
+
+import Foundation
+
