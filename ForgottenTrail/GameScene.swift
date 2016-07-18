4d464af670cee7a4ca39271cfbfb8ef2464de5c4
Matt <Matt@matthews-macbook-pro.local>
2016-07-18 17:55:23 -0400

    swipe character

--- ForgottenTrail/GameScene.swift
+++ ForgottenTrail/GameScene.swift
@@ -8,8 +8,33 @@
 
 import SpriteKit
 
+var you : SKSpriteNode!
+
+
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
+        
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+
+        you = SKSpriteNode(imageNamed: "pirateBack")
+        self.addChild(you)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(GameScene.respondToSwipeGesture(_:)))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view!.addGestureRecognizer(swipeRight)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(GameScene.respondToSwipeGesture(_:)))
+        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+        self.view!.addGestureRecognizer(swipeDown)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(GameScene.respondToSwipeGesture(_:)))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view!.addGestureRecognizer(swipeLeft)
+
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(GameScene.respondToSwipeGesture(_:)))
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        self.view!.addGestureRecognizer(swipeUp)
+
     
     }
     
@@ -19,5 +44,37 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            switch swipeGesture.direction {
+                
+            case UISwipeGestureRecognizerDirection.Right:
+                you.removeFromParent()
+                you = SKSpriteNode(imageNamed: "pirateRight")
+                self.addChild(you)
+                
+            case UISwipeGestureRecognizerDirection.Down:
+                you.removeFromParent()
+                you = SKSpriteNode(imageNamed: "pirateFront")
+                self.addChild(you)
+                
+            case UISwipeGestureRecognizerDirection.Left:
+                you.removeFromParent()
+                you = SKSpriteNode(imageNamed: "pirateLeft")
+                self.addChild(you)
+                
+            case UISwipeGestureRecognizerDirection.Up:
+                you.removeFromParent()
+                you = SKSpriteNode(imageNamed: "pirateBack")
+                self.addChild(you)
+                
+            default:
+                break
+            }
+        }
     }
 }
