320c874a9b152cc3e9bab06bf50d32f4844445f7
Michael Timpson <Michael@michaels-mbp.home>
2016-07-23 22:36:25 -0400

    add

--- ForgottenTrail/Hero.swift
+++ ForgottenTrail/Hero.swift
@@ -26,11 +26,12 @@ enum HeroType: String {
     case Soldier2 = "soldier2"
     case Traveler0 = "traveler0"
     case Traveler1 = "traveler1"
-    //case SilverSci = "silverSci"
+    case SilverSci = "silverSci"
     case Banker0 = "banker"
     
-    static let allHeros = [GoldKnight, IcePrincess, Orc, Soldier0, Princess0, DarkElf0, DarkElf1, Pirate0, Pirate1, Princess1, Princess2, Skeleton, Soldier1, Soldier2, Traveler0, Traveler1, Banker0]
-    
+    static let startingHeros = [Traveler0, Traveler1, IcePrincess, GoldKnight]
+    static let villans = [Orc, Skeleton, SilverSci]
+    static let genericHeros = [Soldier0, Soldier1, Soldier2, Princess0, Princess1, Princess2, DarkElf0, DarkElf1, Pirate0, Pirate1]
 }
 
 
